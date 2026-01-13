8beb9f7d3c60b893409ed02d841b73c0988899fe
naganorikiya <b1023167@fun.ac.jp>
2026-01-13 22:32:28 +0900

    駐車判定

---
+++ Assets/Scripts/ParkingArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ENTER: " + other.name);
+    }
+}
